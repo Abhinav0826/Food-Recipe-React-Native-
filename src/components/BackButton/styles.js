3703059bd40520f2388f1c922203b17745160a63
Abhinav Konda <nagamanoharkonda@Abhis-MacBook.local>
2025-03-11 10:52:18 +0530

    food recipe

---
+++ src/components/BackButton/styles.js
@@ -0,0 +1,29 @@
+// BackButton/styles.js
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  btnContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20, 
+    padding: 10, 
+    paddingTop: 15,
+    margin: 8, 
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 3,
+  },
+  btnIcon: {
+    height: 20, 
+    width: 20,
+  
+  },
+});
+
+export default styles;
